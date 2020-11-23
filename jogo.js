f8722b7e6dbbc361e92ad523e2026cce4d907081
Kevin Leopoldino <kevinleopoldino@gmail.com>
2020-11-23 15:48:28 -0300

    v1

---
+++ jogo.js
@@ -0,0 +1,53 @@
+
+var altura = 0
+var largura = 0
+function ajustaTamanhoPalcoJogo(){
+ altura = window.innerHeight
+ largura = window.innerWidth
+
+ console.log(altura,largura)
+}
+ajustaTamanhoPalcoJogo()
+
+
+function posicaoRandomica(){
+	var posicaoX = Math.floor(Math.random()*altura) -90
+	var posicaoY =Math.floor(Math.random()*largura)-90
+
+	 posicaoX  =  posicaoX < 0 ? 0 :  posicaoX 
+	 posicaoY  =  posicaoY < 0 ? 0 :  posicaoY 
+
+	var mosquito = document.createElement('img')
+	mosquito.src ='imagens/mosquito.png'
+	mosquito.className = tamanhoRandom() + ' '+ladoRandom()
+	mosquito.style.left = posicaoX +'px'
+	mosquito.style.top = posicaoY +'px'
+	mosquito.style.position = 'absolute'
+
+	document.body.appendChild(mosquito)
+}
+
+function tamanhoRandom(){
+	var tamanho = Math.floor(Math.random()*3)
+	switch(tamanho){
+		case 0:
+			return 'mosquito1'
+
+		case 1:
+			return 'mosquito2'
+
+		case 2:
+			return 'mosquito3'
+
+	}
+
+}
+function ladoRandom(){
+	var lado = Math.floor(Math.random()*2)
+	switch(lado){
+		case 0:
+		return 'ladoA'
+		case 1:
+		return 'ladoB'
+	}
+}
